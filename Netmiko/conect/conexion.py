c2ac61b399ae284267726203eadb77e0552de456
Guillermo Sánchez Pastén <wmemo81@gmail.com>
2020-08-10 23:46:11 -0500

    pequeña correccion colector netmiko

--- Netmiko/conect/conexion.py
+++ Netmiko/conect/conexion.py
@@ -3,27 +3,24 @@ import string
 import sys
 from netmiko import ConnectHandler
 #python3.6 -m pip install netmiko
-
 def ejecucion():
     com = sys.argv[1]
     modo=sys.argv[2]
     usuario=str(sys.argv[3])
     contrasenia=str(sys.argv[4])
-
     cisco = {
         'device_type': 'cisco_ios',
         'host': '192.168.23.1',
         'username': usuario,
         'password': contrasenia,
         'port': 22,  # optional, defaults to 22
-        'secret': contrasenia  # optional, defaults to ''
+        'secret': contrasenia
     }
     net_connect = ConnectHandler(**cisco)
     net_connect.enable()
     if modo=="configuracion":
         output = net_connect.send_config_set(com.split("**"))
         print(output)
-
     else:
         output = net_connect.send_command(com)
         print(output)
